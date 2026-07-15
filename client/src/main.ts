1e5febd3e3537868bd48b3628077846394a55f50
hpenney2 <24281994+hpenney2@users.noreply.github.com>
2026-07-15 15:26:35 -0500

    feat(ui): set orientation lock for mobile devices

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,8 +3,9 @@ import "./main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import AuthFail from "./components/AuthFail.vue";
-import { discordSdk, setupPromise, auth as discordAuth } from "./discord.ts";
+import { discordSdk, setupPromise } from "./discord.ts";
 import { socket } from "./socket.ts";
+import { Common } from "@discord/embedded-app-sdk";
 
 function mountError(err: unknown) {
   createApp(AuthFail, { err }).mount("#app");
@@ -13,6 +14,12 @@ function mountError(err: unknown) {
 async function main() {
   try {
     await setupPromise;
+    discordSdk.commands.setOrientationLockState({
+      lock_state: Common.OrientationLockStateTypeObject.LANDSCAPE,
+      picture_in_picture_lock_state: Common.OrientationLockStateTypeObject.LANDSCAPE,
+      grid_lock_state: Common.OrientationLockStateTypeObject.LANDSCAPE,
+    });
+
     socket.connect();
   } catch (err) {
     console.error("error during auth!", JSON.stringify(err));
